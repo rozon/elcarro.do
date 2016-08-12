501a1313ac722759ec5b3a3e3f466e316eaa9d30
Dawlin Peña Luciano <dawmusic19@gmail.com>
2016-08-11 23:43:35 -0400

    Adding old models.

---
+++ PorPartes.Web/Models/Old/ItemType.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PorPartes.Models
+{
+    [Table("Item_Type")]
+    public class ItemType
+    {
+        public ItemType()
+        {
+            Items = new HashSet<Item>();
+        }
+
+        public int ID { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string Type { get; set; }
+
+        public virtual ICollection<Item> Items { get; set; }
+    }
+}
