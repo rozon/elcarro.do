40c7a9e23ab06a55b13ea09e774b9725dc4f21cb
Anthony Rozon <arozonm@gmail.com>
2016-08-15 00:30:11 -0400

    Added Privacy Policy to signup forms and  added required fields

--- js/init.js
+++ js/init.js
@@ -5,6 +5,7 @@
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
     $('select').material_select();
+    $('.modal-trigger').leanModal();
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
