c73c3cec0ce6a07c15cb736b8b7ac091cf6e1b94
Anthony Rozon <arozonm@gmail.com>
2016-08-14 22:55:13 -0400

    Initial Front-End Commit

---
+++ js/init.js
@@ -0,0 +1,10 @@
+(function($){
+  $(function(){
+
+    $('select').material_select('destroy');
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+    $('select').material_select();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
