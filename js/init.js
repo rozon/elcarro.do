8b5abb7304d0a8cfa6a13c81b01502dbe65f3335
Anthony Rozon <arozonm@gmail.com>
2016-08-20 22:23:35 -0400

    Added official logo and improved navbar responsiveness

--- js/init.js
+++ js/init.js
@@ -1,11 +1,13 @@
 (function($){
   $(function(){
 
+    $('.scrollspy').scrollSpy();
     $('select').material_select('destroy');
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
     $('select').material_select();
     $('.modal-trigger').leanModal();
+    $(".button-collapse").sideNav();
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
