ddca33e5fdb98482365c67b3ad485de7db84b2b4
Elian Cruz <eliancruz29@gmail.com>
2016-08-21 19:27:55 -0400

    elcarro-combinating of the desing and funtionalities of the layout

---
+++ ElCarro.Web/Controllers/ModalsController.cs
@@ -0,0 +1,33 @@
+﻿using System.Web.Mvc;
+
+namespace ElCarro.Web.Controllers
+{
+    public class ModalsController : Controller
+    {
+        // GET: Modals
+        public PartialViewResult PrivacyPolicy()
+        {
+            return PartialView("PrivacyPolicy");
+        }
+
+        public PartialViewResult TermsConditions()
+        {
+            return PartialView("TermsConditions");
+        }
+
+        public PartialViewResult Rules()
+        {
+            return PartialView("Rules");
+        }
+
+        public PartialViewResult Suggestions()
+        {
+            return PartialView("Suggestions");
+        }
+
+        public PartialViewResult BugsReport()
+        {
+            return PartialView("BugsReport");
+        }
+    }
+}
