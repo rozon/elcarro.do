627fdac4914541ebcd2f8259342c48aa568c92c5
Anthony Rozon <arozonm@gmail.com>
2016-08-21 01:11:43 -0400

    Added search results table

--- js/init.js
+++ js/init.js
@@ -9,5 +9,16 @@
     $('.modal-trigger').leanModal();
     $(".button-collapse").sideNav();
 
+    $('.chips-initial').material_chip({
+      data: [{
+        tag: '1998',
+      }, {
+        tag: 'Honda',
+      }, {
+        tag: 'Civic',
+      }],
+      placeholder: 'Añade más filtros',
+    });
+
   }); // end of document ready
 })(jQuery); // end of jQuery name space
